447bbf8ac3309916275d275e1ec7c93acf96df5d
dylan+rhoen <dylan+rhoen@appacademy.io>
2015-04-17 14:01:26 -0400

    fine-tune login process; add sign in and out buttons

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  
+
   def new
     @user = User.new
     render :new
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      #log in TODO
+      log_user_in(@user)
       redirect_to user_url @user
     else
       flash.now[:errors] = @user.errors.full_messages
