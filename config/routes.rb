6d141a5824b71b8f84b5679b6753e67009358b13
dylan+rhoen <dylan+rhoen@appacademy.io>
2015-04-17 15:35:28 -0400

    add edit and create buttons for subs, add root route, refactor application layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: redirect('session/new')
   resources :users, only: [:new, :create, :show, :index]
   resource :session, only: [:new, :create, :destroy]
   resources :subs
