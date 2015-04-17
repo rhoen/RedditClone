e81dc94992513899f7f0de8af1805dedb9bb1404
App Academy Student <appacademy@Apps-Mac-mini-27.local>
2015-04-17 11:02:09 -0400

    add token generator to user model and password getter and setter

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,17 @@
 class User < ActiveRecord::Base
+  validates :username, presence: true, uniqueness: true
+  validates :password_digest, presence: true
+  validates :password, length: { minimum: 6 }
+
+  def self.generate_random_token
+    SecureRandom.base64
+  end
+
+  attr_reader :password
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
 end
