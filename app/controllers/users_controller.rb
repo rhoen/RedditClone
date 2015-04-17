a6c99b751c499c6caae059e3ef271ba6f64cb17d
dylan+rhoen <dylan+rhoen@appacademy.io>
2015-04-17 11:11:33 -0400

    define new and create actions for user controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,32 @@
+class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+    render :new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      #log in TODO
+      redirect_to user_url @user
+    else
+      flash[:errors] = @user.errors.full_messages
+      render :new
+    end
+  end
+
+  def show
+
+  end
+
+  def index
+
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:user_name, :password)
+  end
+
+end
