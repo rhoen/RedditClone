ccb8ae2e1913ef19bae17126ca4c8ad7d5006d19
dylan+rhoen <dylan+rhoen@appacademy.io>
2015-04-17 12:32:16 -0400

    add current_user, logged_in?, log_user_in user methods to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,15 @@ class ApplicationController < ActionController::Base
   #protect_from_forgery with: :exception
 
   def current_user
+    User.find_by(session_token: session[:session_token])
+  end
 
+  def logged_in?
+    !!current_user
   end
 
   def log_user_in user
-    
+    session[:session_token] = user.session_token
   end
 
   def user_params
