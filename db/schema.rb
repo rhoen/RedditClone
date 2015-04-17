87dc7a52318e22c4ff78999ca03f7a9b071884f6
dylan+rhoen <dylan+rhoen@appacademy.io>
2015-04-17 14:06:31 -0400

    create subs table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150417161005) do
+ActiveRecord::Schema.define(version: 20150417180316) do
+
+  create_table "subs", force: :cascade do |t|
+    t.string   "title",        null: false
+    t.string   "description",  null: false
+    t.integer  "moderator_id", null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  add_index "subs", ["moderator_id"], name: "index_subs_on_moderator_id"
+  add_index "subs", ["title"], name: "index_subs_on_title", unique: true
 
   create_table "users", force: :cascade do |t|
     t.string   "username",        null: false
