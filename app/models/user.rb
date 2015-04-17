4ac8c3a689ab33963ce704bd7c7c629492eb57ae
dylan+rhoen <dylan+rhoen@appacademy.io>
2015-04-17 12:08:52 -0400

    find users by credentials and verify password

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,11 +7,21 @@ class User < ActiveRecord::Base
     SecureRandom.base64
   end
 
+  def self.find_by_credentials(username, password)
+    user = self.find_by(username: username)
+    return nil if user.nil?
+    user.is_password?(password) ? user : nil
+  end
+
   attr_reader :password
 
   def password=(password)
     @password = password
     self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
   end
 
 end
