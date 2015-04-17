a75dac515e31c645a0b3e6f1f83193881d789552
dylan+rhoen <dylan+rhoen@appacademy.io>
2015-04-17 16:21:55 -0400

    add routes and actions for post

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   root to: redirect('session/new')
   resources :users, only: [:new, :create, :show, :index]
   resource :session, only: [:new, :create, :destroy]
-  resources :subs
+  resources :subs do
+    resources :posts, except: :index, on: :member
+  end
 end
